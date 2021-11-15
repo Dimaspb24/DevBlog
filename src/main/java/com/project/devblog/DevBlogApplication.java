245eaa5e0d6d934f9403ec7418277446f2c5859b
Bogdanov <diman5178@gmail.com>
2021-11-15 23:45:11 +0300

    Init

---
+++ src/main/java/com/project/devblog/DevBlogApplication.java
@@ -0,0 +1,13 @@
+package com.project.devblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/devblog/DevBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.devblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

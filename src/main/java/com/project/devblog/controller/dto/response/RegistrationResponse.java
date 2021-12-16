ef8b3107031cd08945b73c22b176457c391c354b
Aleksey Afanasyev <afanasiev2.aa@edu.spbstu.ru>
2021-12-16 13:27:27 +0300

    :hot_pepper: add RegistrationResponse dto

---
+++ src/main/java/com/project/devblog/controller/dto/response/RegistrationResponse.java
@@ -0,0 +1,12 @@
+package com.project.devblog.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RegistrationResponse {
+
+    private Integer id;
+    private String login;
+}

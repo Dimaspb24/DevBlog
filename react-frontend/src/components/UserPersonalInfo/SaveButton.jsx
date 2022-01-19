ecd0487503f9bc400f0a666169482ddca69b00f3
hoi-bj <d.besedin.0@ya.ru>
2022-01-19 22:31:18 +0300

    Added save button component

---
+++ react-frontend/src/components/UserPersonalInfo/SaveButton.jsx
@@ -0,0 +1,11 @@
+import {Box, Button} from '@mui/material'
+
+const SaveButton = () => {
+    return (
+        <Box>
+            <Button variant="contained" color='secondary'>Сохранить</Button>
+        </Box>
+    )
+}
+
+export default SaveButton

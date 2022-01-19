b89e3b50021f67483b57425f7f4790453af37208
hoi-bj <d.besedin.0@ya.ru>
2022-01-19 18:56:44 +0300

    Added text in user personal info component

---
+++ react-frontend/src/components/UserPersonalInfo/Text.jsx
@@ -0,0 +1,11 @@
+import {Box, Typography} from '@mui/material'
+
+const Text = () => {
+    return (
+        <Box>
+            <Typography sx={{fontSize: '3ch', fontWeight: 600, color: '#512da8'}}>Персональная информация</Typography>
+        </Box>
+    )
+}
+
+export default Text

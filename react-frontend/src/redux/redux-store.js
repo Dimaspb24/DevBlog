9c2bbcc69144bb38ef484e8a2a154977c6a30083
hoi-bj <d.besedin.0@ya.ru>
2022-02-15 21:33:58 +0300

    Added bll for home component

--- react-frontend/src/redux/redux-store.js
+++ react-frontend/src/redux/redux-store.js
@@ -1,10 +1,12 @@
 import {combineReducers, createStore} from 'redux'
 import userPersonalInfoReducer from './user-personal-info-reducer'
 import articleReducer from './article-reducer'
+import homeReducer from './home-reducer'
 
 const reducers = combineReducers({
     userPersonalInfoReducer: userPersonalInfoReducer,
-    articleReducer: articleReducer
+    articleReducer: articleReducer,
+    homeReducer: homeReducer
 })
 
 const store = createStore(reducers)

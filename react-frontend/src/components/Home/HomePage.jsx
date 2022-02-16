84eef1908fa3c64f5a98b35ed5dc0dd948cbc2e3
hoi-bj <d.besedin.0@ya.ru>
2022-02-16 09:12:16 +0300

    Added sort buttons on home component

--- react-frontend/src/components/Home/HomePage.jsx
+++ react-frontend/src/components/Home/HomePage.jsx
@@ -1,11 +1,18 @@
 import {Box} from '@mui/material'
 import UserArticle from '../Article/UserArticle'
+import SortButton from './SortButton'
 
 const HomePage = (props) => {
     const articles = props.articles.map(article => <UserArticle article={article} key={article.id}/>)
 
     return (
         <Box>
+            <Box sx={{display: 'flex', py: '2ch'}}>
+                <SortButton title="По убыванию даты"/>
+                <SortButton title="По возрастанию даты"/>
+                <SortButton title="По убыванию рейтинга"/>
+                <SortButton title="По возрастанию рейтинга"/>
+            </Box>
             {articles}
         </Box>
     )

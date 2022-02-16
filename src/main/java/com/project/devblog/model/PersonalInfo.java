9be5e5f35b5f5f41eadeee74e7b62598d1903c8e
DMITRII BOGDANOV <47987412+Dimaspb24@users.noreply.github.com>
2022-02-16 17:05:09 +0300

    * fix (#33)
    fix Access-Control-Expose-Headers

--- src/main/java/com/project/devblog/model/PersonalInfo.java
+++ src/main/java/com/project/devblog/model/PersonalInfo.java
@@ -1,6 +1,10 @@
 package com.project.devblog.model;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.Embeddable;
